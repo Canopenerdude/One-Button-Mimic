34d0d8a102c3ea4c3089c6722ec1f7e354402579
Canopenerdude <44471513+Canopenerdude@users.noreply.github.com>
2025-11-08 18:17:18 -0500

    THE CODE WORKS

--- Mimic/Assets/scripts/objMove.cs
+++ Mimic/Assets/scripts/objMove.cs
@@ -6,26 +6,89 @@ using UnityEngine;
 public class objMove : MonoBehaviour
 {
     private Rigidbody _rb;
-    public float moveSpeed = 5f;
-    private Vector3 _movementVector;
+
+    public bool movin;
+    public bool rotatin;
+    public bool inspected;
+    public bool isMimic;
+    public bool waitOver;
+    public float speed;
+    public float rotateSpeed;
+    public float waitTime;
+
+    public GameObject mimic;
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 60;
         _rb = GetComponent<Rigidbody>();
+        movin = true;
+        rotatin = false;
+        isMimic = false;
+        if (speed == 0)
+        {
+            speed = .0005f;
+        }
+        if (rotateSpeed == 0)
+        {
+            rotateSpeed = .1f;
+        }
+        if (waitTime == 0)
+        {
+            waitTime = 15f;
+        }
+        mimic = GameObject.FindGameObjectWithTag("mimic");
     }
 
     // Update is called once per frame
     void Update()
     {
-        _movementVector = new Vector3(0, 0, 1).normalized * moveSpeed;
+        if (movin)
+        {
+            _rb.transform.position += new Vector3(0, 0, 1f) * speed;
+        }
+        else
+        {
+            rotatin = true;
+        }
+
+        if (rotatin)
+        {
+            _rb.transform.Rotate(0, rotateSpeed, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && movin && !inspected)
+        {
+            StartCoroutine(Inspect());
+            StartCoroutine(Waiting());
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && rotatin)
+        {
+            isMimic = true;
+        }
+
+        if (isMimic)
+        {
+            Destroy(mimic);
+        }
     }
 
-    private void FixedUpdate()
+    private IEnumerator Inspect()
     {
-        _rb.velocity = new Vector3(_movementVector.x, _rb.velocity.y, _movementVector.z);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _rb.velocity = new Vector3(0, 0, 0);
-        }
+        movin = false;
+        yield return new WaitForSeconds(.5f);
+        rotatin = true;
+        yield return new WaitWhile(() => !isMimic && !waitOver);
+        rotatin = false;
+        _rb.transform.rotation = Quaternion.Euler(0, 0, 0);
+        movin = true;
+        inspected = true;
+    }
+
+    private IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(waitTime);
+        waitOver = true;
     }
 }
