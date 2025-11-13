7d1d4eb2b07f9859c18f6b3beeb969719055dcb0
Canopenerdude <44471513+Canopenerdude@users.noreply.github.com>
2025-11-12 22:27:58 -0500

    scripts and rigids

--- Mimic/Assets/scripts/objMove.cs
+++ Mimic/Assets/scripts/objMove.cs
@@ -18,24 +18,31 @@ public class objMove : MonoBehaviour
     public float speed;
     public float rotateSpeed;
     public float waitTime;
+    public int score;
 
     private int _rInt;
     private string _rString;
+
+    private Vector3 _prevPos;
+
+    public GameObject fire;
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
+        score = 0;
+        fire.transform.position = new Vector3(2.52f, 10.28f, .57f);
         needNewObj = true;
         movin = true;
         rotatin = false;
         isMimic = false;
         if (speed == 0)
         {
-            speed = .0005f;
+            speed = .009f;
         }
         if (rotateSpeed == 0)
         {
-            rotateSpeed = .1f;
+            rotateSpeed = .5f;
         }
         if (waitTime == 0)
         {
@@ -46,6 +53,20 @@ public class objMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (needNewObj)
+        {
+            //Also need a way to call a new object to come in after an object disappears- whether mimic or not.
+            Random r = new Random();
+            _rInt = r.Next(1, 15);
+            _rString = _rInt.ToString();
+            _obj = GameObject.Find("Object"+_rString);
+            _rb = _obj.GetComponent<Rigidbody>();
+            //Also to teleport the objects to the right spot
+            _rb.transform.position = new Vector3(0, 0.222f, -6.5f);
+            needNewObj = false;
+            inspected = false;
+        }
+        
         if (movin)
         {
             _rb.transform.position += new Vector3(0, 0, 1f) * speed;
@@ -57,6 +78,8 @@ public class objMove : MonoBehaviour
 
         if (rotatin)
         {
+            _prevPos = _rb.transform.position;
+            _rb.transform.position = new Vector3(2.13f, 1.64f, 0.53f);
             _rb.transform.Rotate(0, rotateSpeed, 0);
         }
 
@@ -73,22 +96,18 @@ public class objMove : MonoBehaviour
 
         if (isMimic)
         {
-            //Play animation, then do the below
+            StartCoroutine(Fire());
+        }
+
+        if (_rb.transform.position.z > 5.9f)
+        {
             _rb.transform.position = new Vector3(5, 5, -35);
             needNewObj = true;
         }
 
-        if (needNewObj)
+        if (score >= 15)
         {
-            //Also need a way to call a new object to come in after an object disappears- whether mimic or not.
-            Random r = new Random();
-            _rInt = r.Next(1, 15);
-            _rString = _rInt.ToString();
-            _obj = GameObject.Find("Object"+_rString);
-            _rb = _obj.GetComponent<Rigidbody>();
-            //Also to teleport the objects to the right spot
-            _rb.transform.position = new Vector3(0, 0, 1);
-            needNewObj = false;
+            //GoToWinScreen
         }
     }
 
@@ -97,20 +116,30 @@ public class objMove : MonoBehaviour
         movin = false;
         yield return new WaitForSeconds(.5f);
         rotatin = true;
-        yield return new WaitWhile(() => !isMimic && !waitOver);
+        yield return new WaitWhile(() => !waitOver);
         rotatin = false;
         _rb.transform.rotation = Quaternion.Euler(0, 0, 0);
+        _rb.transform.position = _prevPos;
         movin = true;
         inspected = true;
-        //Need a way to disappear the object after it is inspected, if it is not a mimic.
-        yield return new WaitForSeconds(1.5f);
-        _rb.transform.position = new Vector3(5, 5, -35);
-        needNewObj = true;
+        score++;
     }
 
     private IEnumerator Waiting()
     {
         yield return new WaitForSeconds(waitTime);
         waitOver = true;
+    }
+
+    private IEnumerator Fire()
+    {
+        fire.transform.position = new Vector3(2.52f, 5.87f, .57f);
+        yield return new WaitForSeconds(2);
+        _rb.transform.position = new Vector3(5, 5, -35);
+        fire.transform.position = new Vector3(2.52f, 10.28f, .57f);
+        needNewObj = true;
+        isMimic = false;
+        inspected = true;
+        score++;
     }
 }
